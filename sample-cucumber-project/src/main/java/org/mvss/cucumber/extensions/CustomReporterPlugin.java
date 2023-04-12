44ec3c197924712596d15b7e67b195e8f79e985b
Manian VSS <ManianVSS@hotmail.com>
2023-04-12 09:28:35 +0530

    Add more methods to sample cucumber reporting plugin.

--- sample-cucumber-project/src/main/java/org/mvss/cucumber/extensions/CustomReporterPlugin.java
+++ sample-cucumber-project/src/main/java/org/mvss/cucumber/extensions/CustomReporterPlugin.java
@@ -1,14 +1,13 @@
 package org.mvss.cucumber.extensions;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import io.cucumber.plugin.EventListener;
-import io.cucumber.plugin.Plugin;
+import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.*;
 import lombok.extern.log4j.Log4j2;
 import org.mvss.karta.dependencyinjection.utils.ParserUtils;
 
 @Log4j2
-public class CustomReporterPlugin implements Plugin, EventListener {
+public class CustomReporterPlugin implements ConcurrentEventListener {
 
     public CustomReporterPlugin(String parameter) {
         log.info("Plugin parameter passed is " + parameter);
@@ -16,18 +15,31 @@ public class CustomReporterPlugin implements Plugin, EventListener {
 
     @Override
     public void setEventPublisher(EventPublisher publisher) {
+        publisher.registerHandlerFor(TestRunStarted.class, this::handleTestRunStarted);
+
         publisher.registerHandlerFor(TestSourceRead.class, this::handleTestSourceRead);
+        publisher.registerHandlerFor(TestSourceParsed.class, this::handleTestSourceParsed);
+        publisher.registerHandlerFor(StepDefinedEvent.class, this::handleStepDefinedEvent);
+        publisher.registerHandlerFor(SnippetsSuggestedEvent.class, this::handleSnippetsSuggestedEvent);
+
         publisher.registerHandlerFor(TestCaseStarted.class, this::handleTestCaseStarted);
         publisher.registerHandlerFor(TestStepStarted.class, this::handleTestStepStarted);
-        publisher.registerHandlerFor(TestStepFinished.class, this::handleTestStepFinished);
-        publisher.registerHandlerFor(WriteEvent.class, this::handleWrite);
+
         publisher.registerHandlerFor(EmbedEvent.class, this::handleEmbed);
+        publisher.registerHandlerFor(WriteEvent.class, this::handleWrite);
+
+
+        publisher.registerHandlerFor(TestStepFinished.class, this::handleTestStepFinished);
+        publisher.registerHandlerFor(TestCaseFinished.class, this::handleTestCaseFinished);
+
+
         publisher.registerHandlerFor(TestRunFinished.class, this::finishReport);
+
     }
 
     private void printEvent(Event event) {
         try {
-            log.info("Event occurred:" + ParserUtils.getObjectMapper().writeValueAsString(event));
+            log.info("Event of type " + event.getClass() + " occurred:" + ParserUtils.getObjectMapper().writeValueAsString(event));
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
@@ -35,6 +47,22 @@ public class CustomReporterPlugin implements Plugin, EventListener {
 
     private void handleTestSourceRead(TestSourceRead testSourceRead) {
         printEvent(testSourceRead);
+    }
+
+    private void handleTestSourceParsed(TestSourceParsed testSourceParsed) {
+        printEvent(testSourceParsed);
+    }
+
+    private void handleStepDefinedEvent(StepDefinedEvent stepDefinedEvent) {
+        printEvent(stepDefinedEvent);
+    }
+
+    private void handleSnippetsSuggestedEvent(SnippetsSuggestedEvent snippetsSuggestedEvent) {
+        printEvent(snippetsSuggestedEvent);
+    }
+
+    private void handleTestRunStarted(TestRunStarted testRunStarted) {
+        printEvent(testRunStarted);
     }
 
     private void handleTestCaseStarted(TestCaseStarted testCaseStarted) {
@@ -47,6 +75,10 @@ public class CustomReporterPlugin implements Plugin, EventListener {
 
     private void handleTestStepFinished(TestStepFinished testStepFinished) {
         printEvent(testStepFinished);
+    }
+
+    private void handleTestCaseFinished(TestCaseFinished testCaseFinished) {
+        printEvent(testCaseFinished);
     }
 
     private void handleWrite(WriteEvent writeEvent) {
